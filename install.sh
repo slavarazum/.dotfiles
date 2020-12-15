e0a8a5c1f4cce7ceefec680243a68ff2887127a4
Vyacheslav Razum <modern.web.artisan@gmail.com>
2020-12-15 19:40:27 +0200

    Brew install migrated to bash

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ git config --global user.email $GIT_EMAIL
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Update Homebrew recipes
